c586a48d2044e5feb07b36623c7c3dcca34da145
Teslachinator <kantov2001@gmail.com>
2023-03-01 12:30:04 +0300

    Temperature Control

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -9,22 +9,65 @@ function reducer(state, { type, payload }) {
       if (state.overwrite) {
         return {
           ...state,
-          currentOperand: payload,
+          currentOperand: payload.digit,
           overwrite: false,
         };
-      } else if (state.currentOperand === "0" && payload === "0") return state;
-      else if (
-        state.currentOperand === "." &&
-        state.currentOperand.includes(".")
-      ) {
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
+
       return {
         ...state,
-        currentOperand: `${state.currentOperand || ""}${payload}`,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    // if (state.overwrite) {
+    //   return {
+    //     ...state,
+    //     currentOperand: payload,
+    //     overwrite: false,
+    //   };
+    // } else if (state.currentOperand === "0" && payload === "0") return state;
+    // else if (
+    //   state.currentOperand === "." &&
+    //   state.currentOperand.includes(".")
+    // ) {
+    //   return state;
+    // }
+    // return {
+    //   ...state,
+    //   currentOperand: `${state.currentOperand || ""}${payload}`,
+    // };
     case ACTIONS.OPERATION:
-      return {};
+      // return {
+      //   ...state,
+      //   currentOperand: null,
+      //   operation: payload.operation,
+      //   previousOperand: state.currentOperand,
+      // };
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+      break;
     case ACTIONS.CLEAR:
       return {};
     case ACTIONS.DELETE:
@@ -36,10 +79,60 @@ function reducer(state, { type, payload }) {
         currentOperand: state.currentOperand.slice(0, -1),
       };
     case ACTIONS.CALC:
-      return {};
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: calculate(state),
+      };
     default:
       return state;
   }
+}
+
+const calculate = ({ previousOperand, currentOperand, operation }) => {
+  const previ = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  let calc = "";
+  switch (operation) {
+    case "/":
+      calc = previ / current;
+      break;
+    case "+":
+      calc = previ + current;
+      break;
+    case "-":
+      calc = previ - current;
+      break;
+    case "*":
+      calc = previ * current;
+      break;
+    case "+/-":
+      calc = current * -1;
+      break;
+    default:
+      return NaN;
+  }
+  return calc.toString();
+};
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 const Calculator = () => {
@@ -51,18 +144,20 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <div className="display">
-        <div className="history">126+56</div>
-        <div className="output">
-          {previousOperand}
-          {operation}
-          {currentOperand}
+        <div className="history">
+          {formatOperand(previousOperand)} {operation}
         </div>
+        <div className="output">{formatOperand(currentOperand)}</div>
       </div>
       <div className="numPad">
         {btnArr.map((item) => (
           <button
             onClick={() => {
-              dispatch({ type: item.value, payload: item.vol });
+              dispatch({
+                type: item.value,
+                payload: item.vol,
+                key: item.vol,
+              });
             }}
           >
             {item.vol}
--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,14 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Counter.css";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState(230);
+  // const [colorL, setColorL] = useState(50);
+  useEffect(() => {
+    switch (true) {
+      case count === 15:
+        setColor(40);
+        break;
+      case count === 14:
+        setColor(202);
+        break;
+      case count < -12:
+        setColor(250);
+        break;
+      case count > 35:
+        setColor(0);
+      default:
+        break;
+    }
+  }, [count]);
 
   const increment = () => {
+    setColor(color - 2);
     setCount(count + 1);
+    console.log(color);
+
+    // switch (true) {
+    //   case count === 15:
+    //     setColor(40);
+    //     break;
+    //   case count > 15:
+    //     setColor(color - 2);
+    //     break;
+    //   case count >= 0:
+    //     setColor(color - 2);
+    //     console.log(color);
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+
+    // setCount(count + 1);
+    // if (color === 400) {
+    //   setColorL(colorL + 2);
+    // } else if (color === 260) {
+
+    // } else if (count >= -5) {
+    //   setColor(color + 5);
+    // }
+    // if (count === -5) {
+    //   setColorL(50);
+    //   setColor(180);
+    // }
+    // console.log(color);
+    // console.log(colorL);
   };
   const decrement = () => {
     setCount(count - 1);
+    setColor(color + 2);
+    console.log(color);
+    // switch (true) {
+    //   case count === 16:
+    //     setColor(202);
+    //     break;
+    //   case count > 15:
+    //     setColor(color + 2);
+    //     break;
+    //   case count >= 0:
+    //     setColor(color + 2);
+    //     console.log(color);
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    // setCount(count - 1);
+    // if (colorL > 50) {
+    //   setColorL(colorL - 2);
+    // }
+    // if (color === 180) {
+    //   setColorL(colorL + 2);
+    // } else if (count > 41) {
+    //   setColorL(colorL - 2);
+    // } else {
+    //   setColor(color - 5);
+    // }
+    // if (colorL > 80) {
+    //   setColorL(colorL);
+    // }
+    // console.log(color);
+    // console.log(colorL);
   };
 
   return (
@@ -16,8 +101,8 @@ const Counter = () => {
       <div onClick={decrement} class="minus">
         Минус
       </div>
-      <div class="number">
-        <p>{count}</p>
+      <div style={{ background: `hsl(${color}, 100%, 50%)` }} class="number">
+        <p>{count}°C</p>
       </div>
       <div onClick={increment} class="plus">
         Плюс
