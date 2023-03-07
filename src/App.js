bb36bf0c6e1a8000615fe81af61cf4b31f48e414
Teslachinator <kantov2001@mail.ru>
2023-03-07 03:10:02 +0300

    qq

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Calculator from "./calculator/Calculator";
 import Counter from "./counter/Counter";
 import SearchFilter from "./searchData/SearchFilter";
+import Todo from "./Todo-list/Todo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchFilter />
+        <Todo />
       </header>
     </div>
   );
---
+++ src/Todo-list/Todo.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { TiMinus, TiPlus } from "react-icons/ti";
+import "./todo.css";
+
+const Todo = () => {
+  const defState = [{ itemName: "гашик", value: 2, status: false }];
+  const [item, setItem] = useState(defState);
+  const [inputName, setInputName] = useState("");
+
+  const addItem = () => {
+    const newItem = {
+      itemName: inputName,
+      value: 1,
+      status: false,
+    };
+    const addNewItem = [...item, newItem];
+    setItem(addNewItem);
+    setInputName("");
+  };
+
+  const handleQuantityDecrease = (index) => {
+    const newItems = [...item];
+
+    newItems[index].quantity--;
+
+    setItem(newItems);
+  };
+
+  return (
+    <div className="todo">
+      <div className="todo__addItem">
+        <input
+          type="text"
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}
+        />
+        <p onClick={() => addItem()}>Add</p>
+      </div>
+      <button onClick={() => setItem(defState)}>очистить</button>
+      <div className="todo__listItem">
+        {item.map((item, index) => (
+          <div className="listItem__container">
+            <input type="checkbox" name="i" id="1" />
+            <p className="listItem__item">{item.itemName}</p>
+            <div className="item__count">
+              <TiMinus onClick={() => handleQuantityDecrease(index)} />
+              {item.value}
+              <TiPlus onClick={() => setItem([...item][index].value++)} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
